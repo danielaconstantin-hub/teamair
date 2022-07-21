e3a35ae543b63e548ab0622e37385a898267d0e4
rauliathos <rauliathos@gmail.com>
2022-07-21 15:08:39 +0000

    fake commit

--- service1/tests/test1.py
+++ service1/tests/test1.py
@@ -20,3 +20,53 @@ class TestResponse(TestBase):
             response = self.client.get(url_for('home'))
 
         self.assert200(response)
+
+
+class TestRandom(TestBase):
+    def test_get_numbers(self):
+        with patch('random.randint') as r:
+            r.return_value = 0
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'1', response.data)
+        with patch('random.randint') as r:
+            r.return_value = 1
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'2', response.data)
+        with patch('random.randint') as r:
+            r.return_value = 2
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'3', response.data)
+        with patch('random.randint') as r:
+            r.return_value = 3
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'4', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 4
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'5', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 5
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'6', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 6
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'7', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 7
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'8', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 8
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'9', response.data)
+
+        with patch('random.randint') as r:
+            r.return_value = 9
+            response = self.client.get(url_for('/get/letters'))
+            self.assertIn(b'10', response.data)
