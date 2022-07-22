5d10699c02eb81c98df31ae03c114690b19cd64e
rauliathos <rauliathos@gmail.com>
2022-07-22 10:23:17 +0000

    service3 tests 91%

--- service3/tests/test3.py
+++ service3/tests/test3.py
@@ -12,4 +12,12 @@ class TestBase(TestCase):
         return app
 
 class TestResponse(TestBase):
-    def test_5
+    def test_5_and_a(self):
+        with requests_mock.Mocker() as m:
+            with patch('requests.post') as p:
+                m.get('http://localhost:5001/get/numbers', text='5')
+                m.get('http://localhost:5002/get/letters', text='a')
+                p.return_value.text= '5 and a'
+                response= self.client.get(url_for('get_num_let'))
+                self.assertIn(b'5 a', response.data)
+                self.assertIn(b'5 and a', response.data)
