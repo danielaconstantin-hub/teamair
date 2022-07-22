44ad369f0f1c0c254895e3b3bd231649b619137e
Kajan <kajankugananthajothy@gmail.com>
2022-07-22 09:54:12 +0000

    fixed file structure

--- service1/application/routes.py
+++
@@ -1,9 +0,0 @@
-from application import app
-from flask import Flask, Response
-import random
-
-@app.route('/get/numbers', methods=['GET'])
-def get_numbers():
-    number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-    randomnum = random.randint(0,9)
-    return Response(number[randomnum], mimetype='text/plain')
